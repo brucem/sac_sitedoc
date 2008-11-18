30cf8992598fd36db5ee83c55a843454791de1d7
Bruce Morrison <bruce@stuffandcontent.com>
2008-11-18 10:23:44 +1000

    made layout.ini a php file

--- settings/layout.ini.append.php
+++ settings/layout.ini.append.php
@@ -1,9 +1,4 @@
-#?ini charset="iso-8859-1"?
-# eZ publish configuration file for layout module
-# Consists of groups which are the layout name
-# A layout group can have the followin variables
-# - PageLayout - Uses a different pagelayout for this page
-# - SiteDesign - Uses a different sitedesign for this page
-
+<?php /* #?ini charset="utf-8"?
 [sitedoc]
 PageLayout=sitedoc_pagelayout.tpl
+ */ ?>
